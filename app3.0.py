97a846d7986e508a5a6b690bc4452e7642c0ef4e
nicolasct2801 <nicolasct28@hotmail.com>
2024-09-16 11:53:32 -0700

    2ndo commit

--- app3.0.py
+++ app3.0.py
@@ -6,9 +6,13 @@ import streamlit as st
 # Cargar los datos
 car_data = pd.read_csv('vehicles_us3.0.csv')
 
+st.header('Analisis de datos de vehiculos')
+
+
 # Crear un botón para construir un histograma
 hist_button = st.button('Construir histograma')
 if hist_button:
+    print(car_data)
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
@@ -16,6 +20,7 @@ if hist_button:
 # Crear un botón para construir un gráfico de dispersión
 scatter_button = st.button('Construir gráfico de dispersión')
 if scatter_button:
+    print(car_data)
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
